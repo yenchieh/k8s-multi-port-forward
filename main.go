4304e3a228164a738d6f8836e15e92daf150fa23
Jay <yenchiehchen0830@gmai.com>
2020-05-31 16:04:14 -0400

    init commit

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+)
+
+type Service struct {
+	Name string
+	Port string
+}
+
+func (s *Service) ToString() []string {
+	return []string{
+		"port-forward",
+		"svc/" + s.Name,
+		s.Port,
+	}
+}
+
+func main() {
+	services := []Service{
+		{
+			Name: "mongo",
+			Port: "27017:27017",
+		},
+		{
+			Name: "postgres",
+			Port: "5432:5432",
+		},
+		{
+			Name: "rabbitmq",
+			Port: "5672:5672",
+		},
+		{
+			Name: "redis",
+			Port: "6379:6379",
+		},
+	}
+	done := make(chan string)
+	for _, s := range services {
+		v := s
+		go func() {
+			cmd, err := runCmd(v)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if err := cmd.Wait(); err != nil {
+				log.Fatal(err)
+			}
+			done <- fmt.Sprintf("[%s] Closed", v.Name)
+		}()
+	}
+	log.Printf("\nDone: %s\n", <-done)
+
+}
+
+func runCmd(s Service) (*exec.Cmd, error) {
+	cmd, out, err := getCmdReadCloser(s)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(*out)
+	go func() {
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Printf("\n[%s] %s", s.Name, line)
+		}
+	}()
+
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	return cmd, nil
+}
+
+func getCmdReadCloser(s Service) (*exec.Cmd, *io.ReadCloser, error) {
+	c1 := exec.Command("kubectl", s.ToString()...)
+	out, err := c1.StdoutPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return c1, &out, nil
+}
